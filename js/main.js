4cd28c6c35b579989e9d6b7b34e6598d57d6b165
Lohannes <lohannes.s26@gmail.com>
2023-05-14 18:27:24 -0300

    tentativa de carregamento mais rápido

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 'use strict'
 
 import { iniatilizePage } from "./generationList.js";
+import { getGenerationQuantity } from "./fetchs.js";
+import { getGeneration } from "./fetchs.js";
+
+for (let index = 0; index < await getGenerationQuantity().count; index++) {
+    await getGeneration(index)
+}
 
 const routes = {
     '/': '/pages/home.html'
